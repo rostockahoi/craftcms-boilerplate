2cbe850be5e6c7eae90ac6e0458fd3993730e489
Johannes Ahrndt <johannes.ahrndt@rostock-ahoi.de>
2016-11-07 18:13:09 +0100

    Add files

---
+++ src/scripts/app.js
@@ -0,0 +1,5 @@
+var $          = require('../../bower_components/jquery/dist/jquery.js');
+var whatInput  = require('../../bower_components/what-input/what-input.js');
+var foundation = require('../../bower_components/foundation-sites/dist/foundation.js');
+
+$(document).foundation();
