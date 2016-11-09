49f9c0c87ea37f14328ce44f20b15593e3bbfffc
Johannes Ahrndt <johannes.ahrndt@rostock-ahoi.de>
2016-11-09 11:09:35 +0100

    Fix dependency management

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,5 @@
 var $          = require('../../bower_components/jquery/dist/jquery.js');
+global.$       = global.jQuery = jquery;
 var whatInput  = require('../../bower_components/what-input/what-input.js');
 var foundation = require('../../bower_components/foundation-sites/dist/foundation.js');
 
